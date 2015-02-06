6199ec9435fc660955ccd57e21e85dde70efa157
Manuel <manuel@mim.local>
2015-02-06 12:07:14 +0100

    play

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require('express')
-    , app = express()
+  , app = express()
+  , sassMiddleware = require('node-sass-middleware')
 
 ////////////////////////////////////////////////////////////////////////////////
 // Server Views
 app.set('view engine', 'jade')
 app.set('views', __dirname + '/views')
+
+////////////////////////////////////////////////////////////////////////////////
+// Server Styles
+app.use(sassMiddleware({
+    src: __dirname + '/styles',
+    dest: __dirname + '/public',
+    debug: true,
+    outputStyle: 'expanded',
+    force: true
+}));
 
 ////////////////////////////////////////////////////////////////////////////////
 //Middleware
