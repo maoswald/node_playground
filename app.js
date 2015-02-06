6e80543923dadebec43610e6dc402c16af7a437d
Manuel <manuel@mim.local>
2015-02-06 11:14:28 +0100

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+    , app = express()
+
+////////////////////////////////////////////////////////////////////////////////
+// Server Views
+app.set('view engine', 'jade')
+app.set('views', __dirname + '/views')
+
+////////////////////////////////////////////////////////////////////////////////
+//Middleware
+app.use(express.static(__dirname + '/public'))
+
+////////////////////////////////////////////////////////////////////////////////
+// Routes
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
+////////////////////////////////////////////////////////////////////////////////
+//Start server
+app.set('port', process.env.PORT || 3000)
+var server = app.listen(app.get('port'), function() {
+    console.log("Server listing on port %d", server.address().port)
+})
